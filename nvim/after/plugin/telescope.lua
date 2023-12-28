59c9c285aaf484b65d67095ddbd53b864ef2e497
Peter Murphy <petermurphy@perforce.com>
2023-12-28 16:29:56 +0000

    Update NVIM in progress

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,20 +1,20 @@
 local builtin = require('telescope.builtin')
+local wk = require("which-key")
 
 -- Sets the cwd to the directory where Neovim was started
 local start_dir = vim.loop.cwd()
 
 vim.keymap.set('n', '<leader>ff', function()
   builtin.find_files({ cwd = start_dir })
-end, {})
+end, { desc = "Find file" })
 
 vim.keymap.set('n', '<C-p>', function()
   builtin.git_files({ cwd = start_dir })
-end, {})
+end, { desc = "Find file (git repo)" })
 
 vim.keymap.set('n', '<leader>ft', function()
   builtin.grep_string({ search = vim.fn.input("Grep > "), cwd = start_dir })
-end)
+end, { desc = "Find text" })
 
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Find buffer" })
 
